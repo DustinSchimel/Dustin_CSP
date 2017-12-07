4c4374965f442187368dbdc9f10c8fce1f7ad58b
Dustin Schimel <dustin.schimel@gmail.com>
2017-12-07 12:53:39 -0700

    Created art cell class

---
+++ Dustin_CSP/Controller/ArtCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtCellCollectionViewCell.swift
+//  Dustin_CSP
+//
+//  Created by Schimel, Dustin on 12/7/17.
+//  Copyright © 2017 Schimel, Dustin. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCellCollectionViewCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var imageName: UILabel!
+}
