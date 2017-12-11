91e1b7cc6efc7bb87ac944309d75c21a13786652
Dustin Schimel <dustin.schimel@gmail.com>
2017-12-11 13:16:25 -0700

    Completed impact view page with map kit

--- Dustin_CSP/Controller/GlobalImpactViewController.swift
+++ Dustin_CSP/Controller/GlobalImpactViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
 class GlobalImpactView: UIViewController {
 
     @IBOutlet weak var imageFrame: UIImageView!
     @IBOutlet weak var problemLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad()
     {
@@ -19,6 +21,11 @@ class GlobalImpactView: UIViewController {
         imageFrame.image = UIImage(named: "WaterAppPlan")
         problemLabel.text = "The planet that we live on, the people who live on it depend on water. As the human population grows larger the water sources become scarce and dirty. More ways are needed to help people find good sources of water and to educate on proper water usage."
         problemLabel.font = UIFont(name: "Arial", size: 15)
+        
+        let distanceSpan:CLLocationDistance = 250
+        let dirtyWaterLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(14.770910, 120.972185)
+        
+        mapView.setRegion(MKCoordinateRegionMakeWithDistance(dirtyWaterLocation, distanceSpan, distanceSpan), animated: true)
         // Do any additional setup after loading the view.
     }
 
