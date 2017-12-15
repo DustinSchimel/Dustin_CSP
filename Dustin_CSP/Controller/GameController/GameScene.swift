5944917788617802d73e4df3215d554e02492bf9
Dustin Schimel <dustin.schimel@gmail.com>
2017-12-15 13:10:40 -0700

    Changed invader speed

--- Dustin_CSP/Controller/GameController/GameScene.swift
+++ Dustin_CSP/Controller/GameController/GameScene.swift
@@ -14,7 +14,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate
 {
     //MARK: Invader Data
     let rowsOfInvaders : Int = 4
-    var invaderSpeed : Int = 15
+    var invaderSpeed : Double = 30
     var invadersThatCanFire : [Invader] = []
     
     //MARK: Player Data
