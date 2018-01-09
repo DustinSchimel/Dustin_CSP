522477dad1a523cd2aa131cae5f51463015991eb
Dustin Schimel <dustin.schimel@gmail.com>
2018-01-09 13:51:01 -0700

    Put in all the code needed for the data, just need to fix it

--- Dustin_CSP/Controller/DataView/DataViewController.swift
+++ Dustin_CSP/Controller/DataView/DataViewController.swift
@@ -10,27 +10,54 @@ import UIKit
 
 public class DataViewController : UITableViewController
 {
-
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    public override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    lazy var bucketList : [BucketItem] =
+    {
+        return loadBucketListFromFile()
+    }()
+    
+    private func loadBucketListFromFile() -> [BucketItem]
+    {
+        var items = [BucketItem]()
+        if let filePath = Bundle.main.url(forResources: "bucket", withExtension: "csv")
+        {
+            do
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(superatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(speratedBy: ",")
+                    items.append(BucketItem(contents: item[0],author: item[1]))
+                }
+            }
+            catch
+            {
+                print("File load error")
+            }
+        }
+        
+        return items
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK : TableView code
+    
+    override public func numberOfSections(int tableView: UITableView) -> Int
+    {
+        return 1
     }
-    */
-
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSections section: Int) -> Int
+    {
+        return bucketList.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let currentCell = tableView.dequeueReusableCell(withIdentifier: "dataIdenftifier", for: indexPath) as! BucketItemCell
+        
+        currentCell.bucketItem = bucketList[indexPath.row]
+        //      currentCell.bucketItemSignature.text = currentCell.bucketItem.itemAuthor
+        
+        return currentCell
+    }
 }
